863de42268041fbf05cada454b873e3eca63c84b
Dino Perovic <dino.perovic@gmail.com>
2015-02-16 12:26:35 +0100

    add empty test dirs

--- runtests.py
+++ runtests.py
@@ -12,11 +12,15 @@ def main(verbosity=1, *test_args):
     from django_nose import NoseTestSuiteRunner
     test_runner = NoseTestSuiteRunner(verbosity)
 
+    test_apps = ['addresses', 'catalog', 'orders', 'reviews']
+
     if not test_args:
-        test_args = ['tests.catalog']
+        test_args = ['tests.%s' % x for x in test_apps]
+    else:
+        test_args = ['tests.%s' % x for x in test_args if x in test_apps]
 
     failures = test_runner.run_tests(test_args)
     sys.exit(failures)
 
 if __name__ == '__main__':
-    main(1)
+    main(1, *sys.argv[1:])
